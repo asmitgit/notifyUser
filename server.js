14cb4dadbb5dc0e655d7f059b32fade31d510b26
asmit <asmit@policybazaar.com>
2020-04-07 10:41:35 +0530

    v

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.use(cors({ origin: ['notificationsocket.policybazaar.com',
 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://notificationsocket.policybazaar.com");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
 });
